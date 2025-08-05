e44a5a7749a87e5dad4823097773421e032dab76
rishitharaghana <jrishitha2001@gmail.com>
2025-08-05 11:10:50 +0530

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes ,} from 'react-router-dom'
 import Dashboard from './Components/dashboard/Dashboard'
 
 const App = () => {
   return (
    <>
    <BrowserRouter>
-   <Router>
-    <Routes path ="/dashboard" element = {<Dashboard/>}>
-            
-    </Routes>
-   </Router>
+  <Routes>
+    <Route path ="/dashboard" element = {<Dashboard/>}/>
+    
+  </Routes>
    </BrowserRouter>
    </>
   )
