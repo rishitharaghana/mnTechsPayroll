acb45ea7cb76887d1438aa7829347b371a1d8c4c
rishitharaghana <jrishitha2001@gmail.com>
2025-08-16 10:56:05 +0530

    sd

--- src/hooks/AxiosInstance.js
+++ src/hooks/AxiosInstance.js
@@ -5,6 +5,15 @@ const ngrokAxiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+
 });
+ngrokAxiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("Axios error:", error.message);
+    return Promise.reject(error);
+  }
+);
 
 export default ngrokAxiosInstance;
+
