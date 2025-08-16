f3ae07144c6c609dc9a85e6eac5e8d0b91f153a3
rishitharaghana <jrishitha2001@gmail.com>
2025-08-16 11:02:18 +0530

    sd

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,10 @@ export default defineConfig({
     port: 3004,
     host: true,
     strictPort: true,
-    cors: true,
+     cors: {
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+     },
   },
 })
