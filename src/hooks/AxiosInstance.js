07345258f9e215fd61ab599472db0608a080ad95
rishitharaghana <jrishitha2001@gmail.com>
2025-08-16 12:08:23 +0530

    sd

--- src/hooks/AxiosInstance.js
+++ src/hooks/AxiosInstance.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const ngrokAxiosInstance = axios.create({
-  baseURL: "https://3beff8cdc739.ngrok-free.app",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  headers: { "Content-Type": "application/json" },
 });
 
 export default ngrokAxiosInstance;
