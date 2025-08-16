7bca87af201dac0e1ea5f2dc9dd75c510cd524c3
rishitharaghana <jrishitha2001@gmail.com>
2025-08-16 11:44:14 +0530

    sd

--- src/hooks/AxiosInstance.js
+++ src/hooks/AxiosInstance.js
@@ -1,19 +1,10 @@
 import axios from "axios";
 
 const ngrokAxiosInstance = axios.create({
-  baseURL: " https://9f3ce0d92838.ngrok-free.app", 
+  baseURL: "https://3beff8cdc739.ngrok-free.app",
   headers: {
     "Content-Type": "application/json",
   },
-
 });
-ngrokAxiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error("Axios error:", error.message);
-    return Promise.reject(error);
-  }
-);
 
 export default ngrokAxiosInstance;
-
